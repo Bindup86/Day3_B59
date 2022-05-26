b113ebfac466efd474206a7f11a32b743da1028e
Bindup86 <106272529+Bindup86@users.noreply.github.com>
2022-05-26 11:25:50 +0530

    Add files via upload

---
+++ Day3_B59.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Variables in Python 
+
+
+# In[1]:
+
+
+x=10    # declared variables x, y & z
+y=12
+z=x+y
+print(z)
+
+
+# In[ ]:
+
+
+Rules for declaring variables
+
+
+# In[ ]:
+
+
+Rule 1: No name spaces while declaring 
+
+
+# In[4]:
+
+
+# Examples: 
+First name = "Bindu"
+print(First Name)
+
+
+# In[5]:
+
+
+firstname = 'Bindu'
+print(firstname)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+Rule 2: A variable name should not start with a number
+
+
+# In[6]:
+
+
+# Example
+1Name = "Bindu"
+print(1Name)
+
+
+# In[7]:
+
+
+Name1='Bindu'
+print(Name1)
+
+
+# In[8]:
+
+
+Na1me='Bindu'
+print(Na1me)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+Rule 3: A variable name should not contain any special characters.
+
+
+# In[9]:
+
+
+# Example:
+@Name = "Bindu"
+print(@Name)
+
+
+# In[10]:
+
+
+&x = 9
+print(&x)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+Approaches to declare string datatype
+
+
+# In[11]:
+
+
+# Single quote:
+Name = 'Bindu'
+print(Name)
+
+
+# In[12]:
+
+
+# Double quote:
+Name = "Bindu"
+print(Name)
+
+
+# In[13]:
+
+
+# Triple quote:
+Name = """Bindu"""
+print(Name)
+
+
+# In[ ]:
+
+
+
+
